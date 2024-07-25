08cee1103d21f2f6112892f8ddca2bba9330fe41
Lokmane <clashxv5@gmail.com>
2024-07-25 15:50:55 +0200

    Launch bug fix (superposition d'écriture)

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis le nouveau Launcher de la Infinium", "author": "Lokmane" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je charge...", "author": "Lokmane" },
+            { "message": "Le nouveau Launcher de la Infinium", "author": "Lokmane" },
+            { "message": "Infinium on top !", "author": "Lokmane" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
